531d5903518d432c254bbf3595cb8c2a9df2fe30
KhoiNguyen <thorwaitson@gmail.com>
2022-12-15 01:12:34 -0600

    update Wallet.jsx

--- week1/ecdsa-node/client/src/Wallet.jsx
+++ week1/ecdsa-node/client/src/Wallet.jsx
@@ -1,9 +1,19 @@
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({
+  address,
+  setAddress,
+  privateKey,
+  setPrivateKey,
+  balance,
+  setBalance,
+}) {
   async function onChange(evt) {
     const address = evt.target.value;
     setAddress(address);
+
     if (address) {
       const {
         data: { balance },
@@ -19,8 +29,12 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Wallet address:
+        <input
+          placeholder="Wallet address"
+          value={address}
+          onChange={onChange}
+        ></input>
       </label>
 
       <div className="balance">Balance: {balance}</div>
