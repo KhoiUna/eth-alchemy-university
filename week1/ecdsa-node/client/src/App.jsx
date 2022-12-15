7fcd6b45f4f2e64ee1a284036f9322a38a40847d
KhoiNguyen <thorwaitson@gmail.com>
2022-12-14 22:40:02 -0600

    update

--- week1/ecdsa-node/client/src/App.jsx
+++ week1/ecdsa-node/client/src/App.jsx
@@ -2,20 +2,53 @@ import Wallet from "./Wallet";
 import Transfer from "./Transfer";
 import "./App.scss";
 import { useState } from "react";
+import server from "./server";
+
+const { data: balances } = await server.get("/generate-balances");
+const Balance = () => {
+  return (
+    <div className="balances">
+      <u>
+        <h3>Test balances</h3>
+      </u>
+
+      <div>
+        {balances.map(({ privateKey, publicKey, address }) => (
+          <div className="balance_container">
+            <p>
+              <b>Private key:</b> {privateKey}
+            </p>
+            <p>
+              <b>Public key:</b> {publicKey}
+            </p>
+            <p>
+              <b>Address: </b>
+              {address}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 function App() {
   const [balance, setBalance] = useState(0);
-  const [address, setAddress] = useState("");
+  const [signature, setSignature] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
 
   return (
     <div className="app">
       <Wallet
         balance={balance}
         setBalance={setBalance}
-        address={address}
-        setAddress={setAddress}
+        privateKey={privateKey}
+        setPrivateKey={setPrivateKey}
+        address={signature}
+        setAddress={setSignature}
       />
-      <Transfer setBalance={setBalance} address={address} />
+      <Transfer setBalance={setBalance} address={signature} />
+      <Balance />
     </div>
   );
 }
